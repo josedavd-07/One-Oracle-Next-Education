505c1abe12e11bb3278a3f1404c28183b22471ec
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2025-06-18 09:22:30 -0500

    Se añadio el apartado de la tercera sección de Poo de polimorfismo

--- _06_PooPolimorfismo/src/Main.java
+++ _06_PooPolimorfismo/src/Main.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import com.josedavdmast3r.screenmacth.calculos.CalculadoraDeTiempo;
+import com.josedavdmast3r.screenmacth.modelos.Pelicula;
+import com.josedavdmast3r.screenmacth.modelos.Serie;
 
 public class Main {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Enredados");
+        miPelicula.setFechaDeLanzamiento(2010);
+        miPelicula.setDuracionEnMinutos(100);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        Pelicula.showBanner();
+        miPelicula.showInfo();           // Método de instancia
+
+        miPelicula.evaluationFilm(8.5); // Agregar una evaluación
+        miPelicula.evaluationFilm(9.0); // Agregar otra evaluación
+        miPelicula.evaluationFilm(7.5); // Agregar una evaluación más
+
+        System.out.println(miPelicula.getTotalEvaluaciones());
+        System.out.println(miPelicula.calcularMedia());
+
+        System.out.println("\n=====================================\n");
+        // Crear una instancia de Serie
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La Casa del Dragón");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+
+        casaDragon.evaluationFilm(9.5);  // Agregar una evaluación
+        casaDragon.evaluationFilm(9.0);  // Agregar otra evaluación
+        casaDragon.evaluationFilm(8.5);  // Agregar una evaluación más
+
+        Serie.showBanner();
+        casaDragon.showInfo();           // Método de instancia
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        System.out.println("\n******************************************\n");
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("El Rey León");
+        otraPelicula.setFechaDeLanzamiento(1994);
+        otraPelicula.setDuracionEnMinutos(88);
+
+
+
+        System.out.println("\n******************************************\n");
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.printf("Tiempo necesario para ver todas las series es de: %d minutos ",calculadora.getTiempoTotal());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- _06_PooPolimorfismo/src/com/josedavdmast3r/screenmacth/calculos/CalculadoraDeTiempo.java
+++ _06_PooPolimorfismo/src/com/josedavdmast3r/screenmacth/calculos/CalculadoraDeTiempo.java
@@ -1,4 +1,18 @@
 package com.josedavdmast3r.screenmacth.calculos;
+import com.josedavdmast3r.screenmacth.modelos.Titulo;
 
 public class CalculadoraDeTiempo {
+    private int tiempoTotal;
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+
+    // Aplicaremo polimorfismo aquí ya quie una serie o una pelicula es un titulo pero de distinta forma
+    // Por lo tanto, podemos recibir un titulo y calcular su tiempo
+    // Y podemos decir que una serie o una película incluye un título
+
+    public void incluye(Titulo titulo){
+        this.tiempoTotal += titulo.getDuracionEnMinutos();
+    }
 }
